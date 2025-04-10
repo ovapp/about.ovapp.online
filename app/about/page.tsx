d55b94aae8c9308c6318acb18e0d028b1b681801
VerbalThree <verbalthree00@gmail.com>
2025-04-10 14:55:38 -0300

    feat: starting to create the topics

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,14 +6,14 @@ import Image from "next/image"
 export default function Page() {
   return (
 
-    <div>
+    <>
       <Navbar />
 
       <div className={cn(
-    "flex justify-center mt-[5rem] items-center text-center text-[26.5px]",
+    "flex justify-center items-center text-center text-[26.5px] pt-[5rem]",
     interSemi.className,
       )}>
-        <p className="font-normal">
+        <p className="font-normal leading-8">
           Calling those who are
           <span className="block">
         searching for a <b className="text-green-500 underline decoration-dotted">better</b>
@@ -22,7 +22,10 @@ export default function Page() {
             social media
           </span>
         </p>
-        <Image
+      </div>
+
+        <div className="flex justify-center items-center mt-[1rem]">
+      <Image
         src="/ovapp-maskedlogo.png"
         width={0} // 400
         height={0} // 235
@@ -30,11 +33,35 @@ export default function Page() {
         sizes="(max-width: 768px) 100vw, 400px"
         alt="Logo of OpenVoice"
       />
-      
       </div>
 
-      
+      <div className={cn(
+    "flex justify-center items-center text-[26.5px] text-center pt-[1rem]",
+    interSemi.className,
+      )}>
+        <p className="font-normal leading-8">
+          OpenVoice is a social media
+          <span className="block">
+        platform whose priority is <b className="text-blue-500 underline decoration-dotted">data security</b>
+          </span>
+          <span className="block ">
+          and <b className="text-purple-800 underline decoration-dotted">user's privacy</b> 
+          </span>
+        </p>
+      </div>
 
-    </div>
+      <div className="flex justify-center item-center">
+        <h3 className={cn(
+          "font-normal px-[1rem] mt-[2rem]",
+          interSemi.className
+        )}>
+          Theme of this project</h3>
+
+        <div className="px-[1rem]">
+        <hr className="w-[200px] border-t-[2px] border-dotted border-black "/>
+        </div>
+      </div>
+
+    </>
   );
 }
